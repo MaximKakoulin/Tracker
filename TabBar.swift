476f2b05dd53da7e44779abb893cf4692e24cd8d
MaximKakoulin <mximm1213@gmail.com>
2023-06-28 17:47:47 +0700

    added tabBarViewController

---
+++ TabBar.swift
@@ -0,0 +1,41 @@
+//
+//  TabBar.swift
+//  Tracker
+//
+//  Created by Максим on 28.06.2023.
+//
+
+import UIKit
+
+
+final class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createTabBarViewController()
+    }
+
+    private func createTabBarViewController() {
+        tabBar.isTranslucent = false
+        tabBar.barTintColor = .YPBlue
+        tabBar.tintColor = .YPWhite
+
+        let trackerViewController = TabBarViewController()
+        let trackerNavigationController = UINavigationController(rootViewController: trackerViewController)
+
+        trackerViewController.tabBarItem = UITabBarItem(title: "Трекеры",
+                                                       image: UIImage(named: "Record_circle_fill"),
+                                                       selectedImage: nil
+                                                       )
+        trackerViewController.tabBarItem.accessibilityIdentifier = "TrackerView"
+
+        let statisticViewController = TabBarViewController()
+        statisticViewController.tabBarItem = UITabBarItem(title: "Статистика",
+                                                          image: UIImage(named: "Hare_fill"),
+                                                          selectedImage: nil
+                                                          )
+        statisticViewController.tabBarItem.accessibilityIdentifier = "StatisticView"
+
+        self.viewControllers = [trackerNavigationController, statisticViewController]
+    }
+}
