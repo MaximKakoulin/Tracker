ca0c7c806242aadda6fe51042bda6fc7586ea819
MaximKakoulin <mximm1213@gmail.com>
2023-06-28 17:47:27 +0700

    added extension UIColor

---
+++ Helper/Extension UIColor .swift
@@ -0,0 +1,19 @@
+//
+//  Helper.swift
+//  Tracker
+//
+//  Created by Максим on 28.06.2023.
+//
+
+import UIKit
+
+
+extension UIColor {
+    static let YPBackground = UIColor(named: "YPBackground")
+    static let YPRed = UIColor(named: "YPRed")
+    static let YPWhite = UIColor(named: "YPWhite")
+    static let YPBlack = UIColor(named: "YPBlack")
+    static let YPBlue = UIColor(named: "YPBlue")
+    static let YPGrey = UIColor(named: "YPGrey")
+    static let YPLightGrey = UIColor(named: "YPLightGrey")
+}
