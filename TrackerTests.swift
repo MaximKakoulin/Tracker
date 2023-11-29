4231f4294c7f486b384c9d2b7fd4bc6d472ed120
MaximKakoulin <mximm1213@gmail.com>
2023-11-29 22:31:41 +0700

    Added SnapshotTests

--- TrackerTests.swift
+++ TrackerTests.swift
@@ -5,45 +5,31 @@
 //  Created by Максим on 29.11.2023.
 //
 
-//import XCTest
-//import SnapshotTesting
-//@testable import Tracker
-//
-//final class ToDoTrackerTests: XCTestCase {
-//
-//    func testViewController() {
-//            //TODO: - Знаю что нужно делать заглушки, но горят сроки!
-//            let trackerContainer = TrackerPersistentContainer()
-//            let trackerStore = TrackerStore(context: trackerContainer.context)
-//            let trackerCategoryStore = TrackerCategoryStore(
-//                context: trackerContainer.context,
-//                trackerDataStore: trackerStore)
-//            let trackerRecordStore = TrackerRecordStore(context: trackerContainer.context)
-//
-//            let categoryViewModel = CategoryViewModel(
-//                trackerCategoryStore: trackerCategoryStore)
-//
-//            let trackerDataController = TrackerDataController(
-//                trackerCategoryStore: trackerCategoryStore,
-//                trackerStore: trackerStore,
-//                trackerRecordStore: trackerRecordStore,
-//                context: trackerContainer.context)
-//
-//            let appMetric = AppMetrics()
-//
-//            let trackersViewController = TrackersViewController(
-//                trackerDataController: trackerDataController,
-//                trackerCategoryStore: trackerCategoryStore,
-//                categoryViewModel: categoryViewModel,
-//                trackerStore: trackerStore,
-//                trackerRecordStore: trackerRecordStore,
-//                appMetrics: appMetric)
-//
-//            // Для светлой темы
-//            assertSnapshot(matching: trackersViewController, as: .image(traits: .init(userInterfaceStyle: .light)))
-//
-//            // Для темной темы
-//            assertSnapshot(matching: trackersViewController, as: .image(traits: .init(userInterfaceStyle: .dark)), named: "DarkTheme")
-//        }
-//
-//}
+import XCTest
+
+final class TrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
