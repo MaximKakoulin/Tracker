4439198df32f4709c6c02cab45d2423544d1e651
MaximKakoulin <mximm1213@gmail.com>
2023-11-29 18:37:20 +0700

    Added Gradient and extension Array

---
+++ Helpers/Gradient.swift
@@ -0,0 +1,26 @@
+//
+//  Gradient.swift
+//  Tracker
+//
+//  Created by Максим on 29.11.2023.
+//
+
+import UIKit
+
+extension UIImage {
+    static func gradientImage(bounds: CGRect, colors: [UIColor]) -> UIImage {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors.map(\.cgColor)
+
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+
+        return renderer.image { ctx in
+            gradientLayer.render(in: ctx.cgContext)
+        }
+    }
+}
+
